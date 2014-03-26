46004f806422b9786f45a7f02089d200007d3bbd
Jeffrey Zhang <caretcaret@gmail.com>
2014-03-25 21:05:58 -0400

    Add unpack script for windows

---
+++ unpack.py
@@ -0,0 +1,23 @@
+import tarfile
+import os, sys
+
+def untar(filename):
+  try:
+    tar = tarfile.open(filename)
+    tar.extractall()
+    tar.close()
+  except Exception as e:
+    print("Extraction failed:", e)
+
+def usage():
+  print("Usage:\npython unpack.py raw\npython unpack.py processed")
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    usage()
+  elif sys.argv[1].lower().strip() == 'raw':
+    untar('raw.tgz')
+  elif sys.argv[1].lower().strip() == 'processed':
+    untar('processed.tgz')
+  else:
+    usage()
