217db4249fed11af3865352b59ad8d7867b70086
Jeffrey Zhang <caretcaret@gmail.com>
2014-04-02 03:07:05 -0400

    Set up MongoDB server

--- site/main.py
+++ site/main.py
@@ -1,9 +1,15 @@
 from bottle import route, post, run, request, template, static_file, redirect, TEMPLATE_PATH
+from pymongo import Connection
+from bson import json_util
 import os
 
 DEVELOPMENT = True
 HERE = os.path.dirname(os.path.realpath(__file__))
 TEMPLATE_PATH.insert(0, HERE + '/views/')
+DB_URL = 'mongodb://localhost:27017/'
+
+connection = Connection(DB_URL)
+db = connection.concourses
 
 @route('/')
 def home():
@@ -28,9 +34,22 @@ def search():
 def requirements():
   return template('requirements')
 
+# serve this one file statically
+@route('/data/departments')
+def data_departments():
+	return static_file('data/departments.json', root=HERE+'/static')
+
+@route('/data')
+def data():
+	if not request.json:
+		return {}
+
+	result = db.courses.find({'name': {'$regex': 'non-majors', '$options': 'i'}})
+	return json_util.dumps({'courses': result})
+
 @route('/static/<filepath:path>')
 def server_static(filepath):
   return static_file(filepath, root=HERE+'/static')
 
 run(host='localhost', port=int(os.environ.get("PORT", 8080)),
-  reloader=DEVELOPMENT, debug=DEVELOPMENT)
+  server='cherrypy', reloader=DEVELOPMENT, debug=DEVELOPMENT)
